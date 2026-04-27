d292a88ae3cede283904996d4bc36f9a971b288d
harshvardhany87 <harshvardhany87@gmail.com>
2026-04-27 22:47:06 +0900

    Variable and it's types

---
+++ Day1/day1.js
@@ -0,0 +1,16 @@
+// Types of defining variable in JS 
+
+
+//1. 
+num1 = 10;
+num1 = 8 // The compiler always fetches the latest value and ignores the old one.
+console.log(num1);
+
+//2.
+var num2 = 10;
+console.log(num2);
+
+//3.
+let num3 = 10;
+console.log(num3);
+
