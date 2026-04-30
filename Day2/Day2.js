5234c4673553adaa5118cedb2db49d07c6d29c7e
harshvardhany87 <harshvardhany87@gmail.com>
2026-04-30 22:36:15 +0900

    Primitive Data Types

---
+++ Day2/Day2.js
@@ -0,0 +1,14 @@
+let num1 = "Hello";
+console.log("The type is: " + typeof num1);
+
+let num2;
+console.log("The type is: " + typeof num2);
+
+let num3 = 1n;
+console.log("The type is: " + typeof num3);
+
+
+let name1 = "Hello World";
+let name2 = "Welcome to";
+console.log(name2 + " " + name1);
+console.log(name1.toLowerCase());
